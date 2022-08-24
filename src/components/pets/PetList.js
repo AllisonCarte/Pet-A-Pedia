1492808dc7729a3692abbf4da8eb353be4bc91bc
Allison Carte <cartemack@gmail.com>
2022-08-24 13:44:04 -0400

    bare bones petlist

--- src/components/pets/PetList.js
+++ src/components/pets/PetList.js
@@ -6,6 +6,7 @@ export const PetList = () => {
 
     useEffect(
         () => {
+            // fetching what we need from the database. in this case, pets.
             fetch(`http://localhost:8088/pets`)
                 .then(res => res.json())
                 .then((petProfileArray) => {
@@ -18,9 +19,10 @@ export const PetList = () => {
         <h2>Pet Profiles</h2>
         <article className="pets">
             {
+                // iterating array with .map
                 pets.map(
                     pet => {
-                        return <section>
+                        return <section className="pet" key={`pet--${pet.id}`}>
                             <ul>
                                 <div className="image__container"><img className="pet__image" src={pet.image}></img></div>
                                 <h3>{pet.name}</h3>
