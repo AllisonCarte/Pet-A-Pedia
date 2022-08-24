a60e2514c225de5920a459af1e203e304296ea44
Allison Carte <cartemack@gmail.com>
2022-08-24 13:33:44 -0400

    bare bones petlist

--- src/components/pets/PetList.js
+++ src/components/pets/PetList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./Pets.css"
 
 export const PetList = () => {
     const [pets, setPets] = useState([])
@@ -6,10 +7,10 @@ export const PetList = () => {
     useEffect(
         () => {
             fetch(`http://localhost:8088/pets`)
-            .then(res => res.json())
-            .then(() => {
-
-            })
+                .then(res => res.json())
+                .then((petProfileArray) => {
+                    setPets(petProfileArray)
+                })
         },
         []
     )
@@ -20,9 +21,13 @@ export const PetList = () => {
                 pets.map(
                     pet => {
                         return <section>
-                            <header>{pet.name}</header>
-                            <p>{pet.description}</p>
-                            <footer>{pet.notes}</footer>
+                            <ul>
+                                <div className="image__container"><img className="pet__image" src={pet.image}></img></div>
+                                <h3>{pet.name}</h3>
+                                <p>{pet.description}</p>
+                                <footer>{pet.notes}</footer>
+                                <br></br>
+                            </ul>
                         </section>
                     }
                 )
