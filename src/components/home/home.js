efe17b2540051584a1a77039dbaacc207686ef26
Allison Carte <cartemack@gmail.com>
2022-08-29 08:58:34 -0400

    A lot of little things

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import { styles } from "../styles/styles"
+
+
+const PetListWithItems = () => {
+    const [pet, setPet] = useState([])
+
+}
+
 export const Home = () => {
 
     return (
         <>
-            
+
+            <div className="main">
+                <div style={styles.form}className="search">
+                    <input type="text"
+                        placeholder="Search"
+        />
+                </div>
+            </div>
+
 
 
 
