187e9c635a71991a578faf025845b7bb0b1ffb16
Allison Carte <cartemack@gmail.com>
2022-08-29 14:19:37 -0400

    cleaned up a bit

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,4 +1,6 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -10,36 +12,82 @@ export const NavBar = () => {
     const navigate = useNavigate()
     return (
         <Navbar bg="light" expand="lg">
-            <Container>
+            <Container fluid>
                 <Navbar.Brand href="/home">Pet-A-Pedia</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
                         <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href="/profile">Profile</Nav.Link>
-                        <NavDropdown title="Pets" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/pets">My Pets</NavDropdown.Item>
-                            <NavDropdown.Item href="/random">
-                                Random Pet
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="/new">New Profile</NavDropdown.Item>
+                        <NavDropdown title="Pets" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="/pets">My pets</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/liked">
-                                Favorites
+                            <NavDropdown.Item href="/new">
+                                New Profile
                             </NavDropdown.Item>
+
                         </NavDropdown>
                         <Nav.Link href="/" onClick={() => {
                             localStorage.removeItem("PAP_user")
                             navigate("/", { replace: true })
                         }}>Logout</Nav.Link>
                     </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
 }
 
+ // <Navbar bg="light" expand="lg">
+        //     <Container>
+        //         <Navbar.Brand href="/home">Pet-A-Pedia</Navbar.Brand>
+        //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //         <Navbar.Collapse id="basic-navbar-nav">
+        //             <Nav className="me-auto">
+        //                 <Nav.Link href="/home">Home</Nav.Link>
+        //                 <Nav.Link href="/profile">Profile</Nav.Link>
+        //                 <NavDropdown title="Pets" id="basic-nav-dropdown">
+        //                     <NavDropdown.Item href="/pets">My Pets</NavDropdown.Item>
+        //                     {/* <NavDropdown.Item href="/random">
+        //                         Random Pet
+        //                     </NavDropdown.Item> */}
+        //                     <NavDropdown.Divider />
+        //                     <NavDropdown.Item href="/new">New Profile</NavDropdown.Item>
 
+        //                     {/* <NavDropdown.Item href="/liked">
+        //                         Favorites
+        //                     </NavDropdown.Item> */}
+        //                 </NavDropdown>
+        //                 <Nav.Link href="/" onClick={() => {
+        //                     localStorage.removeItem("PAP_user")
+        //                     navigate("/", { replace: true })
+        //                 }}>Logout</Nav.Link>
+        //                 <Form className="d-flex">
+        //                     <Form.Control
+        //                         type="search"
+        //                         placeholder="Search"
+        //                         className="me-2"
+        //                         aria-label="Search"
+        //                     />
+        //                     <Button variant="outline-success">Search</Button>
+        //                 </Form>
+        //             </Nav>
+        //         </Navbar.Collapse>
+        //     </Container>
+        // </Navbar>
 
     // Second way I tried this
 /* <Nav variant="tabs" activeKey="1">
--- src/components/pets/Pets.js
+++ src/components/pets/Pets.js
@@ -1,40 +1,55 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import { styles } from '../styles/styles';
 
 export const Pet = ({ petObject }) => {
 
     return (
-        <Card style={styles.card}>
-            <Card.Img variant="top" src={petObject.image} style={styles.cardImage} />
-            <Card.Body>
-                <Card.Title as="h3">{petObject.name}</Card.Title>
-                <Card.Text>
-                    <div>{petObject.description}</div>
-                    <br></br>
-                    <div>{petObject.notes}</div>
-                   <br></br>
-                    <Button style={styles.button} onClick={""}> ❤️ {petObject.upvotes}</Button>
-                </Card.Text>
-                {/* <Button variant="primary">Go somewhere</Button> */}
-            </Card.Body>
-        </Card>)
+        <Row xs={1} md={4} className="g-4">
+            {Array.from({ length: 1 }).map((_, idx) => (
+                <Col>
+                    <Card >
+                        <Card.Img variant="top" src={petObject.image} />
+                        <Card.Body>
+                            <Card.Title as="h3">{petObject.name}</Card.Title>
+                            <Card.Text>
+                                <div>{petObject.description}</div>
+                                <br></br>
+                                <div>{petObject.notes}</div>
+                                <br></br>
+                                <Button variant="primary" style={styles.button} onClick={""}> ❤️ {petObject.upvotes}</Button>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    );
 }
 
 export const HomePets = ({ petObject }) => {
 
     return (
-        <Card style={styles.card}>
-            <Card.Img variant="top" src={petObject.image} style={styles.cardImage} />
-            <Card.Body>
-                <Card.Title as="h3">{petObject.name}</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <Button style={styles.button} onClick={""}> ❤️ {petObject.upvotes}</Button>
-                </Card.Text>
-                {/* <Button variant="primary">Go somewhere</Button> */}
-            </Card.Body>
-        </Card>)
+        <Row xs={1} md={3} className="g-4">
+            {Array.from({ length: 1 }).map((_, idx) => (
+                <Col>
+                    <Card >
+                        <Card.Img variant="top" src={petObject.image}  />
+                        <Card.Body>
+                            <Card.Title as="h3">{petObject.name}</Card.Title>
+                            <Card.Text>
+                                <br></br>
+                                <Button style={styles.button} onClick={""}> ❤️ {petObject.upvotes}</Button>
+                            </Card.Text>
+                            {/* <Button variant="primary">Go somewhere</Button> */}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    )
 }
 
 /* return <section className="pet" key={`pet--${petObject.id}`}>
